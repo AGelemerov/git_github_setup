435916aee83d080c408655db5a31571bbd801eee
agelemerov <angel.gelemerov@gmail.com>
2022-10-12 11:39:49 +0100

    added some more dict methods

--- Dictionaries.py
+++ Dictionaries.py
@@ -15,10 +15,28 @@ student_1 = {
     "name": "Angel",
     "stream": "DevOps",
     "completed_lessons": 4,
-    "completed_lessons_names": ["lists", "tuples", "strings"]
+    "completed_lessons_names": ["lists", "tuples", "OOP"]
 }
 
-print(type(student_1))
-print(student_1["stream"])  # prints value saved in "stream"
-print(student_1["completed_lessons_names"])
-print(student_1["completed_lessons_names"][0])
+# print(type(student_1))
+# print(student_1["stream"])  # prints value saved in "stream"
+# print(student_1["completed_lessons_names"])
+# print(student_1["completed_lessons_names"][0])
+
+# direct update of element
+student_1["completed_lessons"] = 3
+# print(student_1["completed_lessons"])
+
+# delete and item from list
+# print(student_1["completed_lessons_names"])
+
+# student_1["completed_lessons_names"].remove("OOP")
+# print("Removed OOP")
+
+# print(student_1["completed_lessons_names"])
+
+# print only keys
+print(student_1.keys())
+
+# print only values
+print(student_1.values())
